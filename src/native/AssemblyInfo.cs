781fde398ce67c96c39587088a07a60e59fa92f2
SulthanRaghib <raghib.smp@gmail.com>
2026-08-20 01:47:03 +0700

    feat(metadata): penambahan AssemblyInfo dengan metadata produk resmi Chroniq Screensaver

---
+++ src/native/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Chroniq Screensaver")]
+[assembly: AssemblyDescription("Aesthetic Luxury Analog and Digital Flip Screensaver for Windows")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Chroniq Open Source")]
+[assembly: AssemblyProduct("Chroniq Screensaver")]
+[assembly: AssemblyCopyright("Copyright © 2026 Chroniq Team")]
+[assembly: AssemblyTrademark("Chroniq")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("e5f7a8b9-4c2d-4e6f-8a1b-9c3d5e7f8a9b")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
